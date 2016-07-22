be1b51198ccca8354240a94a0b7b153423693f29
Guillaume Plum <guiplum@gmail.com>
2016-07-22 17:28:27 +0200

    Added some of my configuration file.

---
+++ custom/alias.zsh
@@ -0,0 +1,75 @@
+alias xinit='startx ~/.xinitrc'
+
+alias c='var=$(cal); echo -e "${var/ $(date +%-d) / $(echo -e "\033[01;31m$(date +%-d)\033[00m ")}"'
+
+alias gnuplot='PAGER=less gnuplot'
+alias pylab='/usr/bin/ipython --autoindent --no-banner --pylab=tk'
+alias VerificationShell='ipython --profile=VerificationShell'
+alias yorick='rlwrap -pcyan -c yorick'
+alias urxvt='XMODIFIERS='' urxvt'
+
+alias feh-all='feh --auto-zoom --thumbnails --title'
+alias feh='feh -q -d'
+
+alias fortune='fortune 5% computers 15% ferengi_rules_of_acquisition 10% discworld 20% fr 10% futurama 2% linux 8% science 20% startrek 10% starwars'
+
+alias temperature='sensors && sudo hddtemp /dev/sda'
+alias dfc='dfc -T -o -f -w'
+alias htop='htop || top'
+alias j='jobs -l'
+
+alias h='htop'
+
+alias suspend='systemctl suspend && i3lock -i ~/Images/Wallpapers/DSCN0906.png -t -u -f'
+
+alias su='su -l'
+
+alias ls='ls --color=auto --group-directories-first'
+alias l='ls -CF'
+alias ll='ls -lh'
+alias la='ls -A'
+alias lla='ll -A'
+alias lsnew="ls -rtlh *(D.om[1,10])"
+alias lad='ls -d .*(/)'
+alias lsa='ls -a .*(.)'
+alias lse='ls -d *(/^F)'
+alias lsold="ls -rtlh *(D.Om[1,10])"
+
+alias tree='tree -I "build|*.egg-info|__pycache__"'
+
+alias vi='nvim'
+
+alias yu='yaourt -Syua'
+
+## global aliases (for those who like them) ##
+alias -g '...'='../..'
+alias -g '....'='../../..'
+alias -g BG='& exit'
+alias -g WC='|wc -l'
+alias -g G='|grep'
+alias -g H='|head'
+alias -g Hl=' --help |& less -r'
+#alias -g K='|keep'
+alias -g L='|less'
+alias -g LL='|& less -r'
+alias -g M='|most'
+alias -g N='&>/dev/null'
+#alias -g R='| tr A-z N-za-m'
+alias -g SL='| sort | less'
+alias -g S='| sort'
+alias -g T='|tail'
+alias -g V='| nvim -'
+
+## instead of global aliase it might be better to use grmls $abk assoc array, whose contents are expanded after pressing ,.
+#$abk[SnL]="| sort -n | less"
+
+## get top 10 shell commands:
+#alias top10='print -l ${(o)history%% *} | uniq -c | sort -nr | head -n 10'
+
+## Execute \kbd{./configure}
+#alias CO="./configure"
+
+## Execute \kbd{./configure --help}
+#alias CH="./configure --help"
+
+#vim:ft=zsh
---
+++ custom/divers.zsh
@@ -0,0 +1,93 @@
+source $HOME/Documents/src/Python/vf-py/vf-comp.sh
+
+#Une auto complétion pour les noms d'hôtes ssh :
+if [[ "$SHELL" = "bash" || "$SHELL" = "/bin/bash" ]]
+then
+	if [ -f ~/.ssh/known_hosts ]
+	then
+		SSH_COMPLETE=( $(cat ~/.ssh/known_hosts | \
+			cut -f 1 -d ' ' | \
+			sed -e s/,.*//g | \
+			uniq | \
+			egrep -v [0123456789]) )
+		complete -o default -W "${SSH_COMPLETE[*]}" ssh
+	fi
+fi
+
+
+function ranger-cd {
+    tempfile='/tmp/chosendir'
+    /usr/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    test -f "$tempfile" &&
+    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+        cd -- "$(cat "$tempfile")"
+    fi
+    rm -f -- "$tempfile"
+}
+
+bash() {
+    NO_SWITCH="yes" command bash "$@"
+}
+
+restart () {
+    exec $SHELL $SHELL_ARGS "$@"
+}
+
+plap() {
+    emulate -L zsh
+    if [[ $# = 0 ]] ; then
+        echo "Usage:    $0 program"
+        echo "Example:  $0 zsh"
+        echo "Lists all occurrences of program in the current PATH."
+    else
+        ls -l ${^path}/*$1*(*N)
+    fi
+}
+
+memusage() {
+    ps aux | awk '{if (NR > 1) print $5;
+                   if (NR > 2) print "+"}
+                   END { print "p" }' | dc
+}
+
+## print hex value of a number
+hex() {
+    emulate -L zsh
+    if [[ -n "$1" ]]; then
+        printf "%x\n" $1
+    else
+        print 'Usage: hex <number-to-convert>'
+        return 1
+    fi
+}
+
+vimhelp ()    { vim -c "help $1" -c on -c "au! VimEnter *" }
+
+set_privoxy() {
+	export http_proxy="http://127.0.0.1:8118"
+	export https_proxy="https://127.0.0.1:8118"
+	export ftp_proxy="ftp://127.0.0.1:8118"
+}
+
+unset_privoxy() {
+	unset http_proxy https_proxy ftp_proxy
+}
+
+set_ensta() {
+	export http_proxy="http://proxy.ensta.fr:8080"
+	export https_proxy="https://proxy.ensta.fr:8080"
+	export ftp_proxy="ftp://proxy.ensta.fr:8080"
+}
+
+unset_ensta() {
+	unset http_proxy https_proxy ftp_proxy
+}
+
+function createClassPath() {                                                                                                                                                                    gplum@pc-plum
+        for f in $*
+        do
+                CL="$CL:$f"
+        done
+        echo $CL
+}
+
