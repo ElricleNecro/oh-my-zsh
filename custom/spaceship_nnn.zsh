b94a5cd49bdc8a08d0a1bcd684aeb221e5f6a583
Guillaume Plum <guiplum@gmail.com>
2018-12-23 18:46:27 +0100

    Added nnn support.

---
+++ custom/spaceship_nnn.zsh
@@ -0,0 +1,14 @@
+SPACESHIP_NNN_SHOW="${SPACESHIP_NNN_SHOW:=true}"
+SPACESHIP_NNN_PREFIX="${SPACESHIP_NNN_PREFIX:="(level: "}"
+SPACESHIP_NNN_SUFFIX="${SPACESHIP_NNN_SUFFIX:=") "}"
+
+spaceship_nnn() {
+	[[ $SPACESHIP_NNN_SHOW == false ]] && return
+	([[ -z $SHLVL ]] || [[ $SHLVL -le 1 ]]) && return
+
+	spaceship::section \
+		"red" \
+		"$SPACESHIP_NNN_PREFIX" \
+		"$SHLVL" \
+		"$SPACESHIP_NNN_SUFFIX"
+}
