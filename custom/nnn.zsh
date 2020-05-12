42671e9855399d82f1a1d9be4bc1dab5d68ae575
Guillaume Plum <guiplum@gmail.com>
2020-05-12 15:39:00 +0200

    Added more config for nnn.

--- custom/nnn.zsh
+++ custom/nnn.zsh
@@ -1,2 +1,4 @@
 export NNN_USE_EDITOR=1
-export NNN_BMS="h:~;d:~/Documents;s:~/Documents/src;D:~/Downloads;T:~/Téléchargements"
+export NNN_BMS="h:~;d:~/Documents;s:~/Documents/src;D:~/Downloads;T:~/Téléchargements;g:~/Jeux/GOG/;j:~/Jeux"
+export NNN_OPENER=$(which rifle)
+export NNN_PLUG="y:-_sync*;l:-_git log;x:_chmod +x $nnn"
