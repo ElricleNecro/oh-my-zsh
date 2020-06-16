65e12c1ce2dbd9c2d9c234eadb260e19953a780e
Guillaume Plum <guiplum@gmail.com>
2020-06-16 10:39:26 +0200

    Add fzf functions.

--- custom/divers.zsh
+++ custom/divers.zsh
@@ -22,6 +22,18 @@ then
 	kitty + complete setup zsh | source /dev/stdin
 fi
 
+open_with_fzf() {
+    fd -t f -H -I | fzf -m --preview="xdg-mime query default {}" | xargs -ro -d "\n" xdg-open 2>&-
+}
+
+cd_with_fzf() {
+    cd $HOME && cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
+}
+
+pacs() {
+    sudo pacman -Syy $(pacman -Ssq | fzf -m --preview="pacman -Si {}" --preview-window=:hidden --bind=space:toggle-preview)
+}
+
 function list-theme {
 	for f in ~/.oh-my-zsh/themes/*
 	do
