e5ea1a22b4261a8f09356035c4e8a6e07b893e58
Guillaume Plum <guiplum@gmail.com>
2020-11-16 12:44:46 +0100

    Removed compilers variables.

--- custom/env.zsh
+++ custom/env.zsh
@@ -22,7 +22,7 @@ export MOST_SWITCHES
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;31m'
 export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;33m'   
+export LESS_TERMCAP_so=$'\E[01;33m'
 export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
 export LESS_TERMCAP_ue=$'\E[0m'
@@ -73,11 +73,11 @@ export GOOS
 PYTHONSTARTUP=~/.pythonrc
 export PYTHONSTARTUP
 
-CC=gcc
-export CC
+#CC=gcc
+#export CC
 
-CXX=clang++
-export CXX
+#CXX=clang++
+#export CXX
 
 PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/.local/lib/pkgconfig"
 export PKG_CONFIG_PATH
